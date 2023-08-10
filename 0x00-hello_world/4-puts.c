047a86dfdfd13d2fddeaa52a8c98f1df24174054
9lebch9leb <mohamed.mejimar@e-polytechnique.ma>
2023-08-10 15:25:36 +0100

    4.puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 int main(){
- 
-	char put[]="Programming is like building a multilingual puzzle";
-	puts(put);
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
