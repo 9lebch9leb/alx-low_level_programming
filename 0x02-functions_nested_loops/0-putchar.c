546923ae9fba3c65eaf7ffdf732e0e890e194135
9lebch9leb <mohamed.mejimar@e-polytechnique.ma>
2023-08-16 00:43:32 +0100

    islower

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - printing message followed
+ * by a new line
+ * Return: message with new line
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
